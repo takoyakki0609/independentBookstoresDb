e520336b786f2436b1545c86222b4f3688af8d6b
takoyakki0609 <kjy6478@naver.com>
2024-01-28 22:49:37 +0900

    .

--- routes/independentBookstoreRoutes.js
+++ routes/independentBookstoreRoutes.js
@@ -2,26 +2,27 @@ var express = require("express");
 var router = express.Router();
 const { Pool } = require("pg");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 // Node.js 코드
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Heroku에서 제공하는 데이터베이스 URL
-  ssl: {
-    rejectUnauthorized: false, // SSL 연결 구성 (Heroku 요구 사항에 따라 변경할 수 있음)
-  },
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
-app.get("/bookstores", async (req, res) => {
+app.get("/api/bookstores", async (req, res) => {
   const { lat, lng, radius } = req.query; // radius는 킬로미터 단위로 가정
 
   const query = `
-  SELECT * FROM independentbookstores 
-  WHERE earth_box(ll_to_earth($1, $2), $3 * 1000) @> ll_to_earth(latitude, longitude)
-  AND earth_distance(ll_to_earth($1, $2), ll_to_earth(latitude, longitude)) < $3 * 1000;
-`;
+    SELECT * FROM independentbookstores 
+    WHERE earth_box(ll_to_earth($1, $2), $3 * 1000) @> ll_to_earth(latitude, longitude)
+    AND earth_distance(ll_to_earth($1, $2), ll_to_earth(latitude, longitude)) < $3 * 1000;
+  `;
 
   try {
     const result = await pool.query(query, [lat, lng, radius]);
