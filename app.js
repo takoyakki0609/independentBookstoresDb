3e33a548116509503aebf6315ab385d89a154dac
takoyakki0609 <kjy6478@naver.com>
2024-01-28 18:43:10 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+
+var app = express(); // 이 부분이 누락된 것 같습니다. app 변수를 선언해야 합니다.
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var independentBookstoreRouter = require(path.join(
+  __dirname,
+  "routes",
+  "independentBookstoreRoutes"
+));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(cors({ origin: ["http://localhost:3000"] }));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/bookstores", independentBookstoreRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ routes/independentBookstoreRoutes.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+const { Pool } = require("pg");
+const app = express();
+const port = 8080;
+
+// Node.js 코드
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
+
+app.get("/api/bookstores", async (req, res) => {
+  const { lat, lng, radius } = req.query; // radius는 킬로미터 단위로 가정
+
+  const query = `
+  SELECT * FROM independentbookstores 
+  WHERE earth_box(ll_to_earth($1, $2), $3 * 1000) @> ll_to_earth(latitude, longitude)
+  AND earth_distance(ll_to_earth($1, $2), ll_to_earth(latitude, longitude)) < $3 * 1000;
+`;
+
+  try {
+    const result = await pool.query(query, [lat, lng, radius]);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
+});
+module.exports = router;
